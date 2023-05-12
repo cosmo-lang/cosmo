b8a834b4af7eae4e68c2005643bd0e45733b41bb
R-unic <roleyspersonal@gmail.com>
2023-05-12 12:02:34 -0700

    test: add empty tests for parser + lexer

---
+++ spec/lexer_spec.cr
@@ -0,0 +1,40 @@
+require "./spec_helper"
+
+describe Lexer do
+  it "throws for unexpected characters" do
+
+  end
+  it "lexes floats" do
+
+  end
+  it "lexes integers" do
+
+  end
+  it "lexes booleans" do
+
+  end
+  it "lexes none value" do
+
+  end
+  it "lexes hex literals" do
+
+  end
+  it "lexes binary literals" do
+
+  end
+  it "lexes strings" do
+
+  end
+  it "lexes chars" do
+
+  end
+  it "lexes identifiers" do
+
+  end
+  it "lexes keywords" do
+
+  end
+  it "lexes type keywords" do
+
+  end
+end
---
+++ spec/parser_spec.cr
@@ -0,0 +1,33 @@
+require "./spec_helper"
+
+describe Parser do
+  describe "parses literals" do
+    it "floats" do
+
+    end
+    it "integers" do
+
+    end
+    it "booleans" do
+
+    end
+    it "none" do
+
+    end
+  end
+  it "parses unary operators" do
+
+  end
+  it "parses binary operators" do
+
+  end
+  it "parses variable references" do
+
+  end
+  it "parses variable assignments" do
+
+  end
+  it "parses variable declarations" do
+
+  end
+end
