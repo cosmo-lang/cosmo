1930d06dc86cc2194c3286719d8fc38e7a3fea9c
R-unic <roleyspersonal@gmail.com>
2023-05-12 16:25:46 -0700

    feat: interpret binary ops

--- src/cosmo/runtime/interpreter.cr
+++ src/cosmo/runtime/interpreter.cr
@@ -17,6 +17,39 @@ class Cosmo::Interpreter
     when Statement::Block
       return walk(node.single_expression?.not_nil!) unless node.single_expression?.nil?
       node.nodes.each { |expr| walk(expr) }
+    when Expression::BinaryOp # typechecks
+      left = walk(node.left)
+      right = walk(node.right)
+      case node.operator
+      when Syntax::Plus
+        left + right
+      when Syntax::Minus
+        left - right
+      when Syntax::Star
+        left * right
+      when Syntax::Slash
+        left / right
+      when Syntax::Carat
+        left ** right
+      when Syntax::Percent
+        left % right
+      when Syntax::Ampersand
+        left && right
+      when Syntax::Pipe
+        left || right
+      when Syntax::Less
+        left < right
+      when Syntax::LessEqual
+        left <= right
+      when Syntax::Greater
+        left > right
+      when Syntax::GreaterEqual
+        left >= right
+      when Syntax::EqualEqual
+        left == right
+      when Syntax::BangEqual
+        left != right
+      end
     when
     Expression::IntLiteral,
     Expression::FloatLiteral,
