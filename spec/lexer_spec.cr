43a5eae4ee526c0f8c2b115d6501201b7cacfc85
R-unic <roleyspersonal@gmail.com>
2023-05-12 14:03:17 -0700

    test: implement lexer boolean test

--- spec/lexer_spec.cr
+++ spec/lexer_spec.cr
@@ -2,27 +2,33 @@ require "./spec_helper"
 
 describe Lexer do
   it "throws for unexpected characters" do
-    lexer = Cosmo::Lexer.new("@/\\", "test")
+    lexer = Lexer.new("@/\\", "test")
     expect_raises(Exception, "[1:1] Unexpected character: @") { lexer.tokenize }
   end
   it "lexes floats" do
-    tokens = Cosmo::Lexer.new("1234.4321", "test").tokenize
+    tokens = Lexer.new("1234.4321", "test").tokenize
     tokens.first.type.should eq Syntax::Float
     tokens.first.value.should eq 1234.4321
   end
   it "lexes integers" do
-    tokens = Cosmo::Lexer.new("1234", "test").tokenize
+    tokens = Lexer.new("1234", "test").tokenize
     tokens.first.type.should eq Syntax::Integer
     tokens.first.value.should eq 1234
-  end
-  it "lexes booleans" do
-
   end
   it "lexes hex literals" do
 
   end
   it "lexes binary literals" do
 
+  end
+  it "lexes booleans" do
+    tokens = Lexer.new("true", "test").tokenize
+    tokens.first.type.should eq Syntax::Boolean
+    tokens.first.value.should eq true
+
+    tokens = Lexer.new("false", "test").tokenize
+    tokens.first.type.should eq Syntax::Boolean
+    tokens.first.value.should eq false
   end
   it "lexes none value" do
 
