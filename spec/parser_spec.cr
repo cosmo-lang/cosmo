53ae50f40e2ab6f9b3d89cf38857af0e1f5f110a
R-unic <roleyspersonal@gmail.com>
2023-05-12 15:06:19 -0700

    test: implement unary op parsing test

--- spec/parser_spec.cr
+++ spec/parser_spec.cr
@@ -5,59 +5,64 @@ describe Parser do
     it "floats" do
       block = Parser.new("6.54321", "test").parse
       block.nodes.empty?.should eq false
-      literal = block.nodes.first
-      literal.is_a?(AST::Expression::Literal).should eq true
-      literal.is_a?(AST::Expression::FloatLiteral).should eq true
-      literal.as(AST::Expression::FloatLiteral).value.should eq 6.54321
+      literal = block.nodes.first.as AST::Expression::FloatLiteral
+      literal.class.should eq AST::Expression::FloatLiteral
+      literal.value.should eq 6.54321
     end
     it "integers" do
       block = Parser.new("1234", "test").parse
       block.nodes.empty?.should eq false
-      literal = block.nodes.first
-      literal.is_a?(AST::Expression::Literal).should eq true
-      literal.is_a?(AST::Expression::IntLiteral).should eq true
+      literal = block.nodes.first.as AST::Expression::IntLiteral
+      literal.class.should eq AST::Expression::IntLiteral
       literal.as(AST::Expression::IntLiteral).value.should eq 1234
 
       block = Parser.new("0xABC", "test").parse
       block.nodes.empty?.should eq false
-      literal = block.nodes.first
-      literal.is_a?(AST::Expression::Literal).should eq true
-      literal.is_a?(AST::Expression::IntLiteral).should eq true
+      literal = block.nodes.first.as AST::Expression::IntLiteral
+      literal.class.should eq AST::Expression::IntLiteral
       literal.as(AST::Expression::IntLiteral).value.should eq 2748
 
       block = Parser.new("0b1111", "test").parse
       block.nodes.empty?.should eq false
-      literal = block.nodes.first
-      literal.is_a?(AST::Expression::Literal).should eq true
-      literal.is_a?(AST::Expression::IntLiteral).should eq true
-      literal.as(AST::Expression::IntLiteral).value.should eq 15
+      literal = block.nodes.first.as AST::Expression::IntLiteral
+      literal.class.should eq AST::Expression::IntLiteral
+      literal.value.should eq 15
     end
     it "booleans" do
       block = Parser.new("false", "test").parse
       block.nodes.empty?.should eq false
-      literal = block.nodes.first
-      literal.is_a?(AST::Expression::Literal).should eq true
-      literal.is_a?(AST::Expression::BooleanLiteral).should eq true
-      literal.as(AST::Expression::BooleanLiteral).value.should eq false
+      literal = block.nodes.first.as AST::Expression::BooleanLiteral
+      literal.class.should eq AST::Expression::BooleanLiteral
+      literal.value.should eq false
 
       block = Parser.new("true", "test").parse
       block.nodes.empty?.should eq false
-      literal = block.nodes.first
-      literal.is_a?(AST::Expression::Literal).should eq true
-      literal.is_a?(AST::Expression::BooleanLiteral).should eq true
-      literal.as(AST::Expression::BooleanLiteral).value.should eq true
+      literal = block.nodes.first.as AST::Expression::BooleanLiteral
+      literal.class.should eq AST::Expression::BooleanLiteral
+      literal.value.should eq true
     end
     it "none" do
       block = Parser.new("none", "test").parse
       block.nodes.empty?.should eq false
-      literal = block.nodes.first
-      literal.is_a?(AST::Expression::Literal).should eq true
-      literal.is_a?(AST::Expression::NoneLiteral).should eq true
-      literal.as(AST::Expression::NoneLiteral).value.should eq nil
+      literal = block.nodes.first.as AST::Expression::NoneLiteral
+      literal.class.should eq AST::Expression::NoneLiteral
+      literal.value.should eq nil
     end
   end
   it "parses unary operators" do
+    block = Parser.new("+-12", "test").parse
+    block.nodes.empty?.should eq false
+    unary = block.nodes.first.as AST::Expression::UnaryOp
+    unary.class.should eq AST::Expression::UnaryOp
+    unary.operator.should eq Syntax::Plus
 
+    negate = unary.operand.as AST::Expression::UnaryOp
+    negate.class.should eq AST::Expression::UnaryOp
+    negate.operator.should eq Syntax::Minus
+
+    literal = negate.operand.as AST::Expression::IntLiteral
+    literal.class.should eq AST::Expression::IntLiteral
+    literal.value.should eq 12
   end
   it "parses binary operators" do
 
