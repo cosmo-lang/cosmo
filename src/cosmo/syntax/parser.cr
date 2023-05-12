7cbae48576d4b6f6a84afafc27c5b4b6d993285f
R-unic <roleyspersonal@gmail.com>
2023-05-12 11:10:48 -0700

    feat: implement order of operations in parser

--- src/cosmo/syntax/parser.cr
+++ src/cosmo/syntax/parser.cr
@@ -8,6 +8,7 @@ class Cosmo::Parser
   def initialize(source : String, file_path : String)
     lexer = Lexer.new(source, file_path)
     @tokens = lexer.tokenize
+    @tokens.pop
   end
 
   # Entry point
@@ -16,57 +17,120 @@ class Cosmo::Parser
   end
 
   # Parse an expression and return a node
-  # This is just a simplified example
   private def parse_expression : Node
-    parse_term
+    parse_logical_or
   end
 
-  # Parse a term (e.g., multiplication or division) and return a node
-  private def parse_term : Node
-    left = parse_factor
+  # Parse a logical OR expression and return a node
+  private def parse_logical_or : Node
+    left = parse_logical_and
 
-    case current.type
-    when
-    Syntax::Plus,
-    Syntax::Minus,
-    Syntax::Star,
-    Syntax::Slash,
-    Syntax::Carat,
-    Syntax::Percent,
-    Syntax::Ampersand,
-    Syntax::Pipe,
-    Syntax::Less,
-    Syntax::LessEqual,
-    Syntax::Greater,
-    Syntax::GreaterEqual,
-    Syntax::BangEqual
-      left = parse_binary_op(left)
+    while match?(Syntax::Pipe)
+      op = last_token.type
+      right = parse_logical_and
+      left = Expression::BinaryOp.new(left, op, right)
     end
 
     left
   end
 
-  private def parse_binary_op(left : Node)
-    op = current.type
-    consume(op)
+  # Parse a logical AND expression and return a node
+  private def parse_logical_and : Node
+    left = parse_equality
 
-    right = parse_term
-    Expression::BinaryOp.new(left, op, right)
+    while match?(Syntax::Ampersand)
+      op = last_token.type
+      right = parse_equality
+      left = Expression::BinaryOp.new(left, op, right)
+    end
+
+    left
+  end
+
+  # Parse an equality expression and return a node
+  private def parse_equality : Node
+    left = parse_comparison
+
+    while match?(Syntax::EqualEqual) || match?(Syntax::BangEqual)
+      op = last_token.type
+      right = parse_comparison
+      left = Expression::BinaryOp.new(left, op, right)
+    end
+
+    left
+  end
+
+  # Parse a comparison expression and return a node
+  private def parse_comparison : Node
+    left = parse_addition
+
+    while match?(Syntax::Less) || match?(Syntax::LessEqual) || match?(Syntax::Greater) || match?(Syntax::GreaterEqual)
+      op = last_token.type
+      right = parse_addition
+      left = Expression::BinaryOp.new(left, op, right)
+    end
+
+    left
+  end
+
+  # Parse an addition expression and return a node
+  private def parse_addition : Node
+    left = parse_multiplication
+
+    while match?(Syntax::Plus) || match?(Syntax::Minus)
+      op = last_token.type
+      right = parse_multiplication
+      left = Expression::BinaryOp.new(left, op, right)
+    end
+
+    left
+  end
+
+  # Parse a multiplication expression and return a node
+  private def parse_multiplication : Node
+    left = parse_exponentiation
+
+    while match?(Syntax::Star) || match?(Syntax::Slash) || match?(Syntax::Percent)
+      op = last_token.type
+      right = parse_exponentiation
+      left = Expression::BinaryOp.new(left, op, right)
+    end
+
+    left
+  end
+
+  # Parse an exponentiation expression and return a node
+  private def parse_exponentiation : Node
+    left = parse_unary
+
+    while match?(Syntax::Carat)
+      op = last_token.type
+      right = parse_unary
+      left = Expression::BinaryOp.new(left, op, right)
+    end
+
+    left
+  end
+
+  # Parse a unary expression and return a node
+  private def parse_unary : Node
+    if match?(Syntax::Plus) || match?(Syntax::Minus) || match?(Syntax::Bang) || match?(Syntax::Star)
+      op = last_token.type
+      operand = parse_unary
+      Expression::UnaryOp.new(op, operand)
+    else
+      parse_primary
+    end
   end
 
   # Parse a factor (e.g. number or parentheses) and return a node
   private def parse_factor : Node
     case current.type
     when Syntax::LParen
-      consume(Syntax::LParen)
+      consume_current
       node = parse_expression
       consume(Syntax::RParen)
       node
-    when Syntax::Plus, Syntax::Minus, Syntax::Star, Syntax::Hashtag, Syntax::Bang
-      op = current.type
-      consume(op)
-      operand = parse_factor
-      Expression::UnaryOp.new(op, operand)
     else
       parse_literal
     end
@@ -77,25 +141,36 @@ class Cosmo::Parser
     value = current.value
     case current.type
     when Syntax::Integer
-      consume(current.type)
+      consume_current
       Expression::IntLiteral.new(value.as(Int))
     when Syntax::Float
-      consume(current.type)
+      consume_current
       Expression::FloatLiteral.new(value.as(Float))
     when Syntax::Boolean
-      consume(current.type)
+      consume_current
       Expression::BooleanLiteral.new(value.as(Bool))
     when Syntax::String
-      consume(current.type)
+      consume_current
       Expression::StringLiteral.new(value.to_s)
     when Syntax::Char
-      consume(current.type)
+      consume_current
       Expression::CharLiteral.new(value.as(Char))
     when Syntax::None
-      consume(current.type)
+      consume_current
       Expression::NoneLiteral.new
     else
       raise "Unhandled syntax type: #{current.type}"
+    end
+  end
+
+  # Parse a primary expression and return a node
+  private def parse_primary : Node
+    if match?(Syntax::LParen)
+      node = parse_expression
+      consume(Syntax::RParen)
+      node
+    else
+      parse_literal
     end
   end
 
@@ -104,8 +179,19 @@ class Cosmo::Parser
     @tokens[@position]
   end
 
-  # Consumes the character if it exists, returns whether or not it was consumed
+  # Return the token at the current position minus one
+  private def last_token : Token
+    @tokens[@position - 1]
+  end
+
+  private def finished?
+    @position >= @tokens.size
+  end
+
+  # Consumes the token if the syntax matches, returns whether or not it was consumed
+  # Basically a safe way to consume and see if it was consumed
   private def match?(syntax : Syntax)
+    return false if finished?
     if current.type == syntax
       consume(syntax)
       true
@@ -114,14 +200,15 @@ class Cosmo::Parser
     end
   end
 
-  # Consume the current token if it matches the expected syntax
+  # Consume the current token and advance position if token syntax
+  # matches the expected syntax, else log an error
   private def consume(syntax : Syntax)
     Logger.report_error("Expected #{syntax}, got", current.type.to_s, current.location.line, current.location.position) unless current.type == syntax
     @position += 1
   end
 
   # Consume the current token and advance the position
-  def consume_current : String
+  def consume_current : Token
     token = current
     @position += 1
     token
