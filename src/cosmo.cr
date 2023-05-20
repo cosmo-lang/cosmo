97d65aa0ff85f834ca45e72372ee56dd3e732e72
R-unic <roleyspersonal@gmail.com>
2023-05-19 23:33:51 -0700

    ref: move get_elapsed() into cosmo.cr

--- src/cosmo.cr
+++ src/cosmo.cr
@@ -4,6 +4,14 @@ require "./cosmo/runtime/interpreter"
 require "option_parser"
 require "readline"
 
+# Formats the distance between two time spans into a string
+def get_elapsed(start : Time::Span, finish : Time::Span) : String
+  span = finish - start
+  ms = span.total_milliseconds.round(5)
+  return "#{(ms / 1000).round(2)} seconds"if ms > 1000
+  "#{ms}ms"
+end
+
 module Cosmo
   extend self
 
