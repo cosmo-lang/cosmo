a3e4e0706b4761d631d5127c1e3211f36af6ea8d
R-unic <roleyspersonal@gmail.com>
2023-05-24 20:01:19 -0700

    feat: add Vector->map method

--- src/cosmo/runtime/intrinsic/vector.cr
+++ src/cosmo/runtime/intrinsic/vector.cr
@@ -1,21 +1,44 @@
-class Cosmo::VectorIntrinsics(T)
+class Cosmo::VectorIntrinsics
   def initialize(
     @interpreter : Interpreter,
-    @cache : Array(T)
+    @cache : Array(ValueType)
   )
   end
 
   def get_method(name : Token) #: IntrinsicFunction
     case name.lexeme
     when "filter"
-      Filter(T).new(@interpreter, @cache)
+      Filter.new(@interpreter, @cache)
+    when "map"
+      Map.new(@interpreter, @cache)
     else
       Logger.report_error("Invalid vector method", name.lexeme, name)
     end
   end
 
-  class Filter(T) < IntrinsicFunction
-    def initialize(interpreter : Interpreter, @_self : Array(T))
+  class Map < IntrinsicFunction
+    def initialize(interpreter : Interpreter, @_self : Array(ValueType))
+      super interpreter
+    end
+
+    def arity : Range(UInt32, UInt32)
+      1.to_u..1.to_u
+    end
+
+    def call(args : Array(ValueType)) : Array(ValueType)
+      TypeChecker.assert("func", args.first, token("Vector->map"))
+      fn = args.first.as(Callable)
+
+      res = [] of ValueType
+      @_self.each do |v|
+        res << fn.call([v])
+      end
+      res
+    end
+  end
+
+  class Filter < IntrinsicFunction
+    def initialize(interpreter : Interpreter, @_self : Array(ValueType))
       super interpreter
     end
 
@@ -26,11 +49,8 @@ class Cosmo::VectorIntrinsics(T)
     def call(args : Array(ValueType)) : Array(ValueType)
       TypeChecker.assert("func", args.first, token("Vector->filter"))
       fn = args.first.as(Callable)
-      i = 0
       @_self.select do |v|
-        keep = fn.call([v, i])
-        i += 1
-        keep
+        fn.call([v])
       end
     end
   end
