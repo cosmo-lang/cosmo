8f91db8d8ffc02c94481e54b879dfb5805c29d43
R-unic <roleyspersonal@gmail.com>
2023-06-06 06:44:34 -0700

    test: check for pkg/beginner_codes when running code_challenges folder

--- spec/interpreter_spec.cr
+++ spec/interpreter_spec.cr
@@ -602,7 +602,7 @@ describe Interpreter do
   describe "interprets examples:" do
     example_files = Dir.entries "examples/"
     example_files.each do |example_file|
-      next if example_file.includes?("code_challenges") && !File.exists?("pkg")
+      next if example_file.includes?("code_challenges") && !File.exists?("pkg/beginner_codes")
       interpret_example(File.join "examples", example_file)
     end
   end
