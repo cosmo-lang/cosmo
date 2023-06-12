00e5b10094bb267105317dd55364ddcb427f5041
R-unic <roleyspersonal@gmail.com>
2023-06-12 14:39:54 -0700

    fix: put File functions under FileLib

--- src/cosmo/runtime/intrinsic/lib/file.cr
+++ src/cosmo/runtime/intrinsic/lib/file.cr
@@ -15,95 +15,95 @@ module Cosmo
 
         @i.declare_intrinsic("string->Function", "File", file)
       end
-    end
 
-    class Empty < IFunction
-      def arity : Range(UInt32, UInt32)
-        1.to_u .. 1.to_u
-      end
+      class Empty < IFunction
+        def arity : Range(UInt32, UInt32)
+          1.to_u .. 1.to_u
+        end
 
-      def call(args : Array(ValueType)) : Bool
-        begin
-          File.empty?(args.first.to_s)
-        rescue File::NotFoundError
-          Logger.report_error("Failed to read from file", "File '#{args.first.to_s}' could not be found", token("File->empty?"))
+        def call(args : Array(ValueType)) : Bool
+          begin
+            File.empty?(args.first.to_s)
+          rescue File::NotFoundError
+            Logger.report_error("Failed to read from file", "File '#{args.first.to_s}' could not be found", token("File->empty?"))
+          end
         end
       end
-    end
 
-    class Directory < IFunction
-      def arity : Range(UInt32, UInt32)
-        1.to_u .. 1.to_u
-      end
+      class Directory < IFunction
+        def arity : Range(UInt32, UInt32)
+          1.to_u .. 1.to_u
+        end
 
-      def call(args : Array(ValueType)) : Bool
-        File.directory?(args.first.to_s)
-      end
-    end
-
-    class Exists < IFunction
-      def arity : Range(UInt32, UInt32)
-        1.to_u .. 1.to_u
-      end
-
-      def call(args : Array(ValueType)) : Bool
-        File.exists?(args.first.to_s)
-      end
-    end
-
-    class Delete < IFunction
-      def arity : Range(UInt32, UInt32)
-        1.to_u .. 1.to_u
-      end
-
-      def call(args : Array(ValueType)) : Nil
-
-        begin
-          File.delete(args.first.to_s)
-        rescue File::NotFoundError
-          Logger.report_error("Failed to delete file", "File '#{args.first.to_s}' could not be found", token("File->delete"))
+        def call(args : Array(ValueType)) : Bool
+          File.directory?(args.first.to_s)
         end
       end
-    end
 
-    class Append < IFunction
-      def arity : Range(UInt32, UInt32)
-        2.to_u .. 2.to_u
-      end
+      class Exists < IFunction
+        def arity : Range(UInt32, UInt32)
+          1.to_u .. 1.to_u
+        end
 
-      def call(args : Array(ValueType)) : Nil
-        begin
-          File.write(args.first.to_s, args[1].to_s, mode: "a")
-        rescue File::NotFoundError
-          Logger.report_error("Failed to append to file", "File '#{args.first.to_s}' could not be found", token("File->append"))
+        def call(args : Array(ValueType)) : Bool
+          File.exists?(args.first.to_s)
         end
       end
-    end
 
-    class Write < IFunction
-      def arity : Range(UInt32, UInt32)
-        2.to_u .. 2.to_u
-      end
+      class Delete < IFunction
+        def arity : Range(UInt32, UInt32)
+          1.to_u .. 1.to_u
+        end
 
-      def call(args : Array(ValueType)) : Nil
-        begin
-          File.write(args.first.to_s, args[1].to_s)
-        rescue File::NotFoundError
-          Logger.report_error("Failed to write to file", "File '#{args.first.to_s}' could not be found", token("File->append"))
+        def call(args : Array(ValueType)) : Nil
+
+          begin
+            File.delete(args.first.to_s)
+          rescue File::NotFoundError
+            Logger.report_error("Failed to delete file", "File '#{args.first.to_s}' could not be found", token("File->delete"))
+          end
         end
       end
-    end
 
-    class Read < IFunction
-      def arity : Range(UInt32, UInt32)
-        1.to_u .. 1.to_u
+      class Append < IFunction
+        def arity : Range(UInt32, UInt32)
+          2.to_u .. 2.to_u
+        end
+
+        def call(args : Array(ValueType)) : Nil
+          begin
+            File.write(args.first.to_s, args[1].to_s, mode: "a")
+          rescue File::NotFoundError
+            Logger.report_error("Failed to append to file", "File '#{args.first.to_s}' could not be found", token("File->append"))
+          end
+        end
       end
 
-      def call(args : Array(ValueType)) : String
-        begin
-          File.read(args.first.to_s)
-        rescue File::NotFoundError
-          Logger.report_error("Failed to read from file", "File '#{args.first.to_s}' could not be found", token("File->append"))
+      class Write < IFunction
+        def arity : Range(UInt32, UInt32)
+          2.to_u .. 2.to_u
+        end
+
+        def call(args : Array(ValueType)) : Nil
+          begin
+            File.write(args.first.to_s, args[1].to_s)
+          rescue File::NotFoundError
+            Logger.report_error("Failed to write to file", "File '#{args.first.to_s}' could not be found", token("File->append"))
+          end
+        end
+      end
+
+      class Read < IFunction
+        def arity : Range(UInt32, UInt32)
+          1.to_u .. 1.to_u
+        end
+
+        def call(args : Array(ValueType)) : String
+          begin
+            File.read(args.first.to_s)
+          rescue File::NotFoundError
+            Logger.report_error("Failed to read from file", "File '#{args.first.to_s}' could not be found", token("File->append"))
+          end
         end
       end
     end
